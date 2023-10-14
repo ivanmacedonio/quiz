8e3fd82a3c437aa32dfb48551dbcc7ba0dd9c881
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-10-14 14:54:33 -0300

    no-score-sys

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import { CategoryList } from "../components/CategoryList";
+import '../components/c.css'
+export const HomePage = () => {
+  return (
+    <div className="container-todo">
+      <CategoryList />
+    </div>
+  );
+};
